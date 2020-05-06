d4164e06933c7ba14025e9b3db9dec546002ca7e
zrthstr <zrth1k@gmail.com>
2020-05-06 22:19:15 +0200

    fetch and clean file

--- main.go
+++ main.go
@@ -2,23 +2,31 @@ package main
 
 import (
 	"fmt"
-	"net/http"
-	"os"
 	"io"
+	//"io/ioutil"
+	"net/http"
+	"bufio"
+	"os"
+	"regexp"
 )
 
-func main(){
-	fmt.Println("hello")
-	if err := GetTLDS(); err != nil {
+// curl $TLDURL ; grep -v '^$' | grep -v '//' | sed 's/.*//' | sort | uniq > out
+
+func main() {
+	fileUrl := "https://publicsuffix.org/list/effective_tld_names.dat"
+	filePathRaw := "tld_raw.lst"
+	filePathClean := "tld_clean.lst"
+	if err := GetTLD(fileUrl, filePathRaw); err != nil {
+		panic(err)
+	}
+	//if err := CleanTLDFile(filePathRaw, filePathClean); err != nil {
+	if err := CleanTLDFileX(filePathRaw, filePathClean); err != nil {
 		panic(err)
 	}
 }
 
-func GetTLDS() error {
-	fileUrl := "https://publicsuffix.org/list/effective_tld_names.dat"
-	filePath := "tld.dat"
-
-	resp, err := http.Get(fileUrl)
+func GetTLD(url string, filePath string) error {
+	resp, err := http.Get(url)
 	if err != nil {
 		return err
 	}
@@ -32,4 +40,48 @@ func GetTLDS() error {
 
 	_, err = io.Copy(out, resp.Body)
 	return err
+}
+
+func CleanTLDFileX(fileInPath string, fileOutPath string)(error){
+	// Clean the zone file
+	// get rid of trailing wild char '.*'
+	// get rid of commented lines i.e. line starting with //
+	// get rid of empty lines
+
+	fileIn, err := os.Open(fileInPath)
+	if err != nil {
+		return err
+	}
+	defer fileIn.Close()
+
+	fileOut, err := os.Create(fileOutPath)
+	if err != nil {
+		return err
+	}
+	defer fileOut.Close()
+
+	scanner := bufio.NewScanner(fileIn)
+	wild := regexp.MustCompile(`\*\.`)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		line = wild.ReplaceAllString(line, "")
+		match, _ := regexp.MatchString("//", line)
+		if match {
+			continue
+		}
+		match, _ = regexp.MatchString("^$",line)
+		if match {
+			continue
+		}
+		// l, err := f.WriteString("Hello World")
+		lineN := line + "\n"
+		fileOut.WriteString(lineN)
+		//fmt.Println(line)
+	}
+	if err := scanner.Err(); err != nil {
+		return err
+	}
+	fmt.Println(fileOutPath)
+	return nil
 }
