b9689dd81f45ffeb4f5c159e22fbf5ea33122799
zrthstr <zrth1k@gmail.com>
2020-05-18 23:33:49 +0200

    ..

---
+++ plot/example1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import plotly.express as px
+import numpy as np
+df = px.data.gapminder().query("year == 2007")
+fig = px.sunburst(df, path=['continent', 'country'], values='pop',
+                  color='lifeExp', hover_data=['iso_alpha'],
+                  color_continuous_scale='RdBu',
+                  color_continuous_midpoint=np.average(df['lifeExp'], weights=df['pop']))
+fig.show()
